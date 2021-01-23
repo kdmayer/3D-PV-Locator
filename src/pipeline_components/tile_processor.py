fixed PV polygon coord calculation in polygon creator

src/pipeline_components/tile_processor.py
@@ -12,6 +12,7 @@ from src.utils.polygon_creator import PolygonCreator
 from pathlib import Path
 import torch
 from torchvision import datasets, models, transforms, utils
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 import csv
 import os
 import numpy as np
@@ -105,10 +106,14 @@ class TileProcessor(object):
 
     def __loadSegModel(self):
 
-        # Load DeepLab model from .pt file
-        seg_model = torch.load(self.seg_checkpoint_path, map_location=self.device)
+        seg_model = models.segmentation.deeplabv3_resnet101(pretrained=True, progress=True)
 
-        # Put model into inference mode
+        seg_model.classifier = DeepLabHead(2048, 1)
+
+        checkpoint = torch.load(self.seg_checkpoint_path, map_location=self.device)
+
+        seg_model.load_state_dict(checkpoint['model_state_dict'])
+
         seg_model.eval()
 
         return seg_model
@@ -342,18 +347,19 @@ class TileProcessor(object):
             currentTile = batch[0]
 
             # Try to process and record it
-            try:
+            # try:
 
-                self.__processTiles(currentTile, trans_cls, trans_seg)
+            self.__processTiles(currentTile, trans_cls, trans_seg)
 
-                with open(Path(self.processed_path), "a") as csvFile:
+            with open(Path(self.processed_path), "a") as csvFile:
 
-                    writer = csv.writer(csvFile, lineterminator="\n")
+                writer = csv.writer(csvFile, lineterminator="\n")
 
-                    writer.writerow([currentTile])
+                writer.writerow([currentTile])
 
             # Only tiles that weren't fully processed are saved subsequently
             # ToDo: Catch the exception and write it in a second column in the .csv
+            '''
             except:
 
                 e = sys.exc_info()[0]
@@ -364,7 +370,7 @@ class TileProcessor(object):
                     writer = csv.writer(csvFile, lineterminator="\n")
 
                     writer.writerow([currentTile, e])
-
+            '''
             # Delete iterated tile
             os.remove(Path(self.tile_dir + "/" + str(currentTile)))
 

src/utils/polygon_creator.py
@@ -18,11 +18,10 @@ class PolygonCreator():
     def _deltapx2latlon(self, px_distance):
 
         dist_px_x, dist_px_y = px_distance
-        lat_max, lon_min = self.upper_left_coords
-        lat_new = lat_max - (self.dlat * (dist_px_y/self.size))
-        lon_new = lon_min + (((self.side * 360) / (2 * np.pi * self.earth_radius * np.cos(np.deg2rad(lat_new))))) * (dist_px_x/self.size)
-
-        return (lat_new, lon_new)
+        x_min, y_max = self.upper_left_coords
+        y_new = y_max + (self.side / self.size) * (dist_px_y - 0.5) * (self.dlat / self.side)
+        x_new = x_min + (self.side / self.size) * (dist_px_x - 0.5) * 360 * (1 / (2 * np.pi * self.earth_radius * np.cos(np.deg2rad(y_new))))
+        return (x_new, y_new)
 
     def _polygon2latlon(self, poly_exterior_coords):
 
@@ -40,7 +39,7 @@ class PolygonCreator():
         geos = gpd.GeoDataFrame()
         geos['class'] = None
         geos['geometry'] = None
-        for idx, (shape, value) in enumerate(raster.shapes(mask)):
+        for idx, (shape, value) in enumerate(raster.shapes(mask, transform = (1.0, 0.0, 0.0, 0.0, -1.0, 0.0))):
             polygon = Polygon(shape["coordinates"][0])
             poly_exterior_coords = list(polygon.exterior.coords)
             polygon_latlon = self._polygon2latlon(poly_exterior_coords)